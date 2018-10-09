6bb24d6fec70d796b228a015017f027b53aa3a9a
Shriharsh Mishra <harsh82@gmail.com>
2018-10-09 12:56:06 +0000

    Fill screen implementation added

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,89 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+//Psuedocode 
+//screen_end=SCREEN + 8192
+//
+//(LOOP)
+//    if KBD = 0 goto COLOR_WHITE
+//    else goto COLOR_BLACK
+//
+//(COLOR_BLACK)
+//    color=0
+//    goto FILL
+//
+//(COLOR_WHITE)
+//    color=-1
+//    got FILL
+//
+//    i=SCREEN
+//(FILL)
+//    if (screen_end-i = 0) goto LOOP
+//    else 
+//       M[i]=color
+//       i=i+1
+//       goto FILL
+
+
+    //Initialize screen_end
+    @8192
+    D=A
+    @SCREEN
+    D=D+A
+    @screen_end
+    M=D
+
+
+(LOOP)
+    //Read Keyboard memory 
+    @KBD
+    D=M
+
+    //If KBD=0 goto COLOR_WHITE
+    @COLOR_WHITE
+    D;JEQ
+    
+    //Set color to black (-1)
+    @color
+    M=-1
+    @FILL
+    0;JMP
+
+    //Set color to white (0)
+(COLOR_WHITE)
+    @color
+    M=0
+
+    //Fill the screen memory with value of 'color'
+(FILL)
+    //Initialize i=SCREEN
+    @SCREEN
+    D=A 
+    @i
+    M=D
+
+(FILL_LOOP)
+    //Calculate screen_end - i 
+    @screen_end
+    D=M
+    @i
+    D=D-M
+
+    //Goto LOOP if screen_end - i = 0
+    @LOOP
+    D;JEQ
+
+    //Set screen memory with color value
+    @color
+    D=M
+    @i
+    A=M
+    M=D
+
+    //Increment i 
+    @i
+    M=M+1
+
+    @FILL_LOOP
+    0;JMP
